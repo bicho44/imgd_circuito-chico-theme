fbd4eea25cf0ea7d3d7f646ba8bb6b488778f6eb
Federico <bicho44@gmail.com>
2019-09-06 22:17:59 -0300

    Filtrar las setting del Framework si ya fueron creadas

--- imgd-framework/imgd_settings.php
+++ imgd-framework/imgd_settings.php
@@ -4,20 +4,21 @@
 *
 * @package: IMGD Framework
 */
+if (!function_exists(imgd_plugin_setting_pages)){
+    function imgd_plugin_setting_pages($pages)
+    {
+        $pages[] = array(
+            'page_title' => __('Theme Options','imgd')
+            ,'menu_title' => __('Theme Options', 'imgd')
+            ,'capability' => 'manage_options'
+            ,'menu_slug' => 'opciones_imgd'
+            ,'setting' => 'opciones_imgd'
+            ,'single_line' => true
+            ,'default_tab' => 'Home'
+            ,'save_text' => __('Guardar Cambios','imgd')
+        );
 
-function imgd_plugin_setting_pages($pages)
-{
-    $pages[] = array(
-        'page_title' => __('Theme Options','imgd')
-        ,'menu_title' => __('Theme Options', 'imgd')
-        ,'capability' => 'manage_options'
-        ,'menu_slug' => 'opciones_imgd'
-        ,'setting' => 'opciones_imgd'
-        ,'single_line' => true
-        ,'default_tab' => 'Home'
-        ,'save_text' => __('Guardar Cambios','imgd')
-    );
-
-    return $pages;
+        return $pages;
+    }
+    add_filter('piklist_admin_pages', 'imgd_plugin_setting_pages');
 }
-add_filter('piklist_admin_pages', 'imgd_plugin_setting_pages');
