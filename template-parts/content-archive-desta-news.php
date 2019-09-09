84e6abfce23e6b691e1049d66b747e2f00db5126
Federico <bicho44@gmail.com>
2019-09-09 00:59:59 -0300

    modal tratando que ande en la web

--- template-parts/content-archive-desta-news.php
+++ template-parts/content-archive-desta-news.php
@@ -36,26 +36,28 @@ if (has_post_thumbnail()) {?>
         <?php if($link_compra!=""){ ?>
             <a href="<?php echo $link_compra; ?>" class="btn btn--vermas"><?php echo get_post_meta(get_the_ID(), 'imgd_texto_link_compra_field', true);?></a>
         <?php }; ?>
-    </div>
 
-    <!-- Acá va el Modal de la consulta por <?php the_title();?> -->
-    <section class="modal--show" id="consulta-<?php the_ID(); ?>" tabindex="-1"
-        role="dialog" aria-labelledby="modal-label" aria-hidden="true">
+        <!-- Acá va el Modal de la consulta por <?php the_title();?> -->
+        <section class="modal--show" id="consulta-<?php the_ID(); ?>" tabindex="-1"
+            role="dialog" aria-labelledby="modal-label" aria-hidden="true">
 
-    <div class="modal-inner">
-            <header><h3 id="label-fade">Consulta por <?php the_title();?></h2></header>
-        <div class="modal-content">
-        <?php
-        /**
-         * @todo: hacer un setting para el shortcode
-         * @todo: buscar una solución más elegante a tener que madar un do_shortcode
-         * @todo: Buscar como mandar un mail mas personalizado de consulta por ejemplo el "subject" del mail y
-         *        dentro del cuerpo del mismo algo más adaptado a lo que necesita una consulta
-        */
-        echo do_shortcode('[contact-form-7 id="100" title="consulta"]') ?>
+        <div class="modal-inner">
+                <header><h3 id="label-fade">Consulta por <?php the_title();?></h2></header>
+            <div class="modal-content">
+            <?php
+            /**
+             * @todo: hacer un setting para el shortcode
+             * @todo: buscar una solución más elegante a tener que madar un do_shortcode
+             * @todo: Buscar como mandar un mail mas personalizado de consulta por ejemplo el "subject" del mail y
+             *        dentro del cuerpo del mismo algo más adaptado a lo que necesita una consulta
+            */
+            echo do_shortcode('[contact-form-7 id="100" title="consulta"]') ?>
+            </div>
         </div>
+
+        <a href="#!" class="modal-close" title="Close this modal" data-close="Close"
+            data-dismiss="modal">×</a>
+        </section>
     </div>
 
-    <a href="#!" class="modal-close" title="Close this modal" data-close="Close"
-        data-dismiss="modal">×</a>
-    </section>
+
