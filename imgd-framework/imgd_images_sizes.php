7ec39bbcf2c790efacc36027ade911d8f42b5694
Federico <bicho44@gmail.com>
2019-09-19 18:22:21 -0300

    image sizes

--- imgd-framework/imgd_images_sizes.php
+++ imgd-framework/imgd_images_sizes.php
@@ -15,10 +15,10 @@
 
 add_image_size('full-cropped', 1150, 480, true);
 
-add_image_size('header-cropped', 1150, 300, true, array( 'center', 'top' ) );
+add_image_size('header-cropped', 1150, 300, true, array( 'center', 'top' ));
 
-add_image_size('show-cropped', 340, 340, true);
-add_image_size('show-archive', 750, 200, true);
+// add_image_size('show-cropped', 340, 340, true);
+// add_image_size('show-archive', 750, 200, true);
 
 add_image_size('news-featured', 750, 420, true);
 add_image_size('news-archive', 360, 200, true);
@@ -73,10 +73,11 @@ function img_get_size_thumbnail()
  *                      values in pixels (in that order).
  * @return string A source size value for use in a content image 'sizes' attribute.
  */
-function imgd_content_image_sizes_attr( $sizes, $size ) {
+function imgd_content_image_sizes_attr($sizes, $size)
+{
     $width = $size[0];
     840 <= $width && $sizes = '(max-width: 709px) 85vw, (max-width: 909px) 67vw, (max-width: 1362px) 62vw, 840px';
-    if ( 'page' === get_post_type() ) {
+    if ('page' === get_post_type()) {
         840 > $width && $sizes = '(max-width: ' . $width . 'px) 85vw, ' . $width . 'px';
     } else {
         840 > $width && 600 <= $width && $sizes = '(max-width: 709px) 85vw, (max-width: 909px) 67vw, (max-width: 984px) 61vw, (max-width: 1362px) 45vw, 600px';
@@ -84,7 +85,7 @@ function imgd_content_image_sizes_attr( $sizes, $size ) {
     }
     return $sizes;
 }
-add_filter( 'wp_calculate_image_sizes', 'imgd_content_image_sizes_attr', 10 , 2 );
+add_filter('wp_calculate_image_sizes', 'imgd_content_image_sizes_attr', 10, 2);
 
 /**
  * Add custom image sizes attribute to enhance responsive image functionality
@@ -97,18 +98,19 @@ add_filter( 'wp_calculate_image_sizes', 'imgd_content_image_sizes_attr', 10 , 2 
  * @param array $size Registered image size or flat array of height and width dimensions.
  * @return string A source size value for use in a post thumbnail 'sizes' attribute.
  */
-function imgd_post_thumbnail_sizes_attr( $attr, $attachment, $size ) {
-    if ( 'post-thumbnail' === $size ) {
-        is_active_sidebar( 'sidebar-1' ) && $attr['sizes'] = '(max-width: 709px) 85vw, (max-width: 909px) 67vw, (max-width: 984px) 60vw, (max-width: 1362px) 62vw, 840px';
-        ! is_active_sidebar( 'sidebar-1' ) && $attr['sizes'] = '(max-width: 709px) 85vw, (max-width: 909px) 67vw, (max-width: 1362px) 88vw, 1200px';
+function imgd_post_thumbnail_sizes_attr($attr, $attachment, $size)
+{
+    if ('post-thumbnail' === $size) {
+        is_active_sidebar('sidebar-1') && $attr['sizes'] = '(max-width: 709px) 85vw, (max-width: 909px) 67vw, (max-width: 984px) 60vw, (max-width: 1362px) 62vw, 840px';
+        ! is_active_sidebar('sidebar-1') && $attr['sizes'] = '(max-width: 709px) 85vw, (max-width: 909px) 67vw, (max-width: 1362px) 88vw, 1200px';
     }
     return $attr;
 }
-add_filter( 'wp_get_attachment_image_attributes', 'imgd_post_thumbnail_sizes_attr', 10 , 3 );
+add_filter('wp_get_attachment_image_attributes', 'imgd_post_thumbnail_sizes_attr', 10, 3);
 
 
 
-if(!function_exists('imgd_has_slideshow_thumbnail')) {
+if (!function_exists('imgd_has_slideshow_thumbnail')) {
     /**
      * Verifica que haya imagen en el SlideShow
      *
@@ -121,164 +123,171 @@ if(!function_exists('imgd_has_slideshow_thumbnail')) {
     }
 }
 
-if(!function_exists('imgd_get_slideshow_thumbnail_id')) {
-	/**
-	 * Retrieve SlideShow Image thumbnail ID.
-	 *
-	 * @since 2.9.0
-	 * @since 4.4.0 `$post` can be a post ID or WP_Post object.
-	 *
-	 * @param int|WP_Post $post Optional. Post ID or WP_Post object. Default is global `$post`.
-	 * @return string|int Post thumbnail ID or empty string.
-	 */
+if (!function_exists('imgd_get_slideshow_thumbnail_id')) {
+    /**
+     * Retrieve SlideShow Image thumbnail ID.
+     *
+     * @since 2.9.0
+     * @since 4.4.0 `$post` can be a post ID or WP_Post object.
+     *
+     * @param int|WP_Post $post Optional. Post ID or WP_Post object. Default is global `$post`.
+     * @return string|int Post thumbnail ID or empty string.
+     */
 
-	function imgd_get_slideshow_thumbnail_id($post = null)
-	{
-		$post = get_post($post);
-		if (!$post) {
-			return '';
-		}
-		return get_post_meta($post->ID, 'imgd_image_slideshow', true);
-	}
+    function imgd_get_slideshow_thumbnail_id($post = null)
+    {
+        $post = get_post($post);
+        if (!$post) {
+            return '';
+        }
+        return get_post_meta($post->ID, 'imgd_image_slideshow', true);
+    }
 }
 
-if (!function_exists('get_intermediate_image_names')){
-	/**
-	* Get the list of images Sizes in a array
-	*
-	* @global $_wp_additional_image_sizes
-	* @uses   get_intermediate_image_sizes()
-	* @return array $sizes Data for all currently-registered image sizes.
-	*/
-	function get_intermediate_image_names() {
-		global $_wp_additional_image_sizes;
+if (!function_exists('get_intermediate_image_names')) {
+    /**
+    * Get the list of images Sizes in a array
+    *
+    * @global $_wp_additional_image_sizes
+    * @uses   get_intermediate_image_sizes()
+    * @return array $sizes Data for all currently-registered image sizes.
+    */
+    function get_intermediate_image_names()
+    {
+        global $_wp_additional_image_sizes;
 
-			$sizes = array();
+        $sizes = array();
 
-			foreach ( get_intermediate_image_sizes() as $_size ) {
-			$sizes[ $_size] = $_size;
-			}
-			return $sizes;
-	}
-
-
-	/**
-	 * Get size information for all currently-registered image sizes.
-	 *
-	 * @global $_wp_additional_image_sizes
-	 * @uses   get_intermediate_image_sizes()
-	 * @return array $sizes Data for all currently-registered image sizes.
-	 */
-	function get_image_sizes() {
-		global $_wp_additional_image_sizes;
-
-		$sizes = array();
-
-		foreach ( get_intermediate_image_sizes() as $_size ) {
-			if ( in_array( $_size, array('thumbnail', 'medium', 'medium_large', 'large') ) ) {
-				$sizes[ $_size ]['width']  = get_option( "{$_size}_size_w" );
-				$sizes[ $_size ]['height'] = get_option( "{$_size}_size_h" );
-				$sizes[ $_size ]['crop']   = (bool) get_option( "{$_size}_crop" );
-			} elseif ( isset( $_wp_additional_image_sizes[ $_size ] ) ) {
-				$sizes[ $_size ] = array(
-					'width'  => $_wp_additional_image_sizes[ $_size ]['width'],
-					'height' => $_wp_additional_image_sizes[ $_size ]['height'],
-					'crop'   => $_wp_additional_image_sizes[ $_size ]['crop'],
-				);
-			}
-		}
-
-		return $sizes;
-	}
+        foreach (get_intermediate_image_sizes() as $_size) {
+            $sizes[ $_size] = $_size;
+        }
+        return $sizes;
+    }
 
 
+    /**
+     * Get size information for all currently-registered image sizes.
+     *
+     * @global $_wp_additional_image_sizes
+     * @uses   get_intermediate_image_sizes()
+     * @return array $sizes Data for all currently-registered image sizes.
+     */
+    function get_image_sizes()
+    {
+        global $_wp_additional_image_sizes;
 
-	/**
-	 * Get size information for a specific image size.
-	 *
-	 * @uses   get_image_sizes()
-	 * @param  string $size The image size for which to retrieve data.
-	 * @return bool|array $size Size data about an image size or false if the size doesn't exist.
-	 */
-	function get_image_size( $size ) {
-		$sizes = get_image_sizes();
+        $sizes = array();
 
-		if ( isset( $sizes[ $size ] ) ) {
-			return $sizes[ $size ];
-		}
+        foreach (get_intermediate_image_sizes() as $_size) {
+            if (in_array($_size, array('thumbnail', 'medium', 'medium_large', 'large'))) {
+                $sizes[ $_size ]['width']  = get_option("{$_size}_size_w");
+                $sizes[ $_size ]['height'] = get_option("{$_size}_size_h");
+                $sizes[ $_size ]['crop']   = (bool) get_option("{$_size}_crop");
+            } elseif (isset($_wp_additional_image_sizes[ $_size ])) {
+                $sizes[ $_size ] = array(
+                    'width'  => $_wp_additional_image_sizes[ $_size ]['width'],
+                    'height' => $_wp_additional_image_sizes[ $_size ]['height'],
+                    'crop'   => $_wp_additional_image_sizes[ $_size ]['crop'],
+                );
+            }
+        }
 
-		return false;
-	}
+        return $sizes;
+    }
 
-	/**
-	 * Get the width of a specific image size.
-	 *
-	 * @uses   get_image_size()
-	 * @param  string $size The image size for which to retrieve data.
-	 * @return bool|string $size Width of an image size or false if the size doesn't exist.
-	 */
-	function get_image_width( $size ) {
-		if ( ! $size = get_image_size( $size ) ) {
-			return false;
-		}
 
-		if ( isset( $size['width'] ) ) {
-			return $size['width'];
-		}
 
-		return false;
-	}
+    /**
+     * Get size information for a specific image size.
+     *
+     * @uses   get_image_sizes()
+     * @param  string $size The image size for which to retrieve data.
+     * @return bool|array $size Size data about an image size or false if the size doesn't exist.
+     */
+    function get_image_size($size)
+    {
+        $sizes = get_image_sizes();
 
-	/**
-	 * Get the height of a specific image size.
-	 *
-	 * @uses   get_image_size()
-	 * @param  string $size The image size for which to retrieve data.
-	 * @return bool|string $size Height of an image size or false if the size doesn't exist.
-	 */
-	function get_image_height( $size ) {
-		if ( ! $size = get_image_size( $size ) ) {
-			return false;
-		}
+        if (isset($sizes[ $size ])) {
+            return $sizes[ $size ];
+        }
 
-		if ( isset( $size['height'] ) ) {
-			return $size['height'];
-		}
+        return false;
+    }
 
-		return false;
-	}
+    /**
+     * Get the width of a specific image size.
+     *
+     * @uses   get_image_size()
+     * @param  string $size The image size for which to retrieve data.
+     * @return bool|string $size Width of an image size or false if the size doesn't exist.
+     */
+    function get_image_width($size)
+    {
+        if (! $size = get_image_size($size)) {
+            return false;
+        }
+
+        if (isset($size['width'])) {
+            return $size['width'];
+        }
+
+        return false;
+    }
+
+    /**
+     * Get the height of a specific image size.
+     *
+     * @uses   get_image_size()
+     * @param  string $size The image size for which to retrieve data.
+     * @return bool|string $size Height of an image size or false if the size doesn't exist.
+     */
+    function get_image_height($size)
+    {
+        if (! $size = get_image_size($size)) {
+            return false;
+        }
+
+        if (isset($size['height'])) {
+            return $size['height'];
+        }
+
+        return false;
+    }
 } //Get Intermedite Image Names
 
-if ( ! function_exists( 'imgd_post_thumbnail' ) ) :
-	/**
-	 * Displays an optional post thumbnail.
-	 *
-	 * Wraps the post thumbnail in an anchor element on index views, or a div
-	 * element when on single views.
-	 */
-	function imgd_post_thumbnail() {
-		// if ( ! twentynineteen_can_show_post_thumbnail() ) {
-		// 	return;
-		// }
+if (! function_exists('imgd_post_thumbnail')) :
+    /**
+     * Displays an optional post thumbnail.
+     *
+     * Wraps the post thumbnail in an anchor element on index views, or a div
+     * element when on single views.
+     */
+    function imgd_post_thumbnail()
+    {
+        // if ( ! twentynineteen_can_show_post_thumbnail() ) {
+        // 	return;
+        // }
 
-		if ( is_singular() ) :
-			?>
+        if (is_singular()) :
+            ?>
 
-			<figure class="post-thumbnail">
-				<?php the_post_thumbnail(); ?>
-			</figure><!-- .post-thumbnail -->
+<figure class="post-thumbnail">
+	<?php the_post_thumbnail(); ?>
+</figure><!-- .post-thumbnail -->
 
-			<?php
-		else :
-			?>
+<?php
+        else :
+            ?>
 
-		<figure class="post-thumbnail">
-			<a class="post-thumbnail-inner" href="<?php the_permalink(); ?>" aria-hidden="true" tabindex="-1">
-				<?php the_post_thumbnail( 'news-archive' ); ?>
-			</a>
-		</figure>
+<figure class="post-thumbnail">
+	<a class="post-thumbnail-inner" href="<?php the_permalink(); ?>"
+		aria-hidden="true" tabindex="-1">
+		<?php the_post_thumbnail('news-archive'); ?>
+	</a>
+</figure>
 
-			<?php
-		endif; // End is_singular().
-	}
+<?php
+        endif; // End is_singular().
+    }
 endif;
