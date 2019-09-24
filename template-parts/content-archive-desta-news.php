dcb6648ca02d32c7e2c2e7fd2c12615b01b4d64c
Federico <bicho44@gmail.com>
2019-09-24 20:55:34 -0300

    estilos formularios

--- template-parts/content-archive-desta-news.php
+++ template-parts/content-archive-desta-news.php
@@ -11,40 +11,46 @@
 <?php
 // Must be inside a loop.
 if (has_post_thumbnail()) {?>
-    <!-- <a href="<?php //the_permalink(); ?>"> -->
-        <?php  /* @todo Cambiar para que la imagen sea responsive */
+<!-- <a href="<?php //the_permalink();?>"> -->
+<?php  /* @todo Cambiar para que la imagen sea responsive */
         the_post_thumbnail('news-archive');
         ?>
-    <!-- </a> -->
+<!-- </a> -->
 <?php } ?>
 
-    <h3>
-        <a href="<?php the_permalink(); ?>" rel="bookmark">
+<h3>
+    <a href="<?php the_permalink(); ?>" rel="bookmark">
         <?php the_title();?>
-        </a>
-    </h3>
+    </a>
+</h3>
 
-    <div class="actividad-caption">
-        <?php shortentext(get_the_content(), 100);  ?>
-    </div>
+<div class="actividad-caption">
+    <?php shortentext(get_the_content(), 100);  ?>
+</div>
 <?php $link_compra = get_post_meta(get_the_ID(), 'imgd_link_compra_field', true); ?>
-    <!-- <pre><?php //var_dump($link_compra);?></pre> -->
+<!-- <pre><?php //var_dump($link_compra);?>
+</pre> -->
 
 
-    <div class="botones">
-        <a href="#consulta-<?php the_ID(); ?>" class="btn btn--consulta"><?php _e('Consulta por esta actividad','imgd'); ?></a>
-        <?php if($link_compra!=""){ ?>
-            <a href="<?php echo $link_compra; ?>" class="btn btn--vermas"><?php echo get_post_meta(get_the_ID(), 'imgd_texto_link_compra_field', true);?></a>
-        <?php }; ?>
+<div class="botones">
+    <a href="#consulta-<?php the_ID(); ?>"
+        class="btn btn--consulta"><?php _e('Consulta por esta actividad', 'imgd'); ?></a>
+    <?php if ($link_compra!="") { ?>
+    <a href="<?php echo $link_compra; ?>"
+        class="btn btn--vermas"><?php echo get_post_meta(get_the_ID(), 'imgd_texto_link_compra_field', true);?></a>
+    <?php }; ?>
 
-        <!-- Acá va el Modal de la consulta por <?php the_title();?> -->
-        <section class="modal--show" id="consulta-<?php the_ID(); ?>" tabindex="-1"
-            role="dialog" aria-labelledby="modal-label" aria-hidden="true">
+    <!-- Acá va el Modal de la consulta por <?php the_title();?> -->
+    <section class="modal--show" id="consulta-<?php the_ID(); ?>"
+        tabindex="-1" role="dialog" aria-labelledby="modal-label" aria-hidden="true">
 
         <div class="modal-inner">
-                <header><h3 id="label-fade">Consulta por <?php the_title();?></h2></header>
+            <header>
+                <h3 id="label-fade">Consulta por <?php the_title();?>
+                    </h2>
+            </header>
             <div class="modal-content">
-            <?php
+                <?php
             /**
              * @todo: hacer un setting para el shortcode
              * @todo: buscar una solución más elegante a tener que madar un do_shortcode
@@ -55,9 +61,6 @@ if (has_post_thumbnail()) {?>
             </div>
         </div>
 
-        <a href="#!" class="modal-close" title="Close this modal" data-close="Close"
-            data-dismiss="modal">×</a>
-        </section>
-    </div>
-
-
+        <a href="#!" class="modal-close" title="Close this modal" data-close="Close" data-dismiss="modal">×</a>
+    </section>
+</div>
