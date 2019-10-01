975159ff2decdd40cb6ed5e8aaad8075a146a5d9
Federico <bicho44@gmail.com>
2019-09-30 21:35:12 -0300

    usar la actividad header para todas las páginas

--- page.php
+++ page.php
@@ -19,18 +19,23 @@ $back_image="";
 
     while (have_posts()) : the_post();
 
-    if (has_post_thumbnail()) {
-        $back_image=get_the_post_thumbnail_url(null, 'full-cropped');
-        //$back_image = ''.$back_image.'\");';
-    }
+    // if (has_post_thumbnail()) {
+    //     $back_image=get_the_post_thumbnail_url(null, 'full-cropped');
+    //     //$back_image = ''.$back_image.'\");';
+    // }
 
     ?>
+<header class="actividad-header">
+    <?php the_title('<h1 class="actividad-titulo">', '</h1>'); ?>
+    <?php the_post_thumbnail('full-cropped', ['class' => 'actividad-imagen']);?>
+    <!-- <img src="/assets/images/actividades/single/combo-deluxe-2017.jpg" alt="" class="actividad-imagen" /> -->
+</header>
 
-<div class="hero-page"
-    style="background-image: url('<?php echo $back_image;?>')" ;>
-    <h1><?php the_title();?>
-    </h1>
-</div>
+<!-- <div class="hero-page"
+    style="background-image: url('<?php //echo $back_image;?>')" ;>
+<h1><?php //the_title();?>
+</h1>
+</div> -->
 
 <div class="page-area">
 
@@ -39,14 +44,11 @@ $back_image="";
             the_content();
 
             //echo wpdocs_custom_taxonomies_terms_links();
-
-
             wp_link_pages(array(
                 'before' => '<div class="page-links">' . esc_html__('Pages:', 'turismointer'),
                 'after'  => '</div>',
             ));
         ?>
-
         <?php
         // If comments are open or we have at least one comment, load up the comment template.
         if (comments_open() || get_comments_number()) :
