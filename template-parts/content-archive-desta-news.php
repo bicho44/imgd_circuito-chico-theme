7270098e7675e58b2f1ee20097d6b6ebdf700556
Federico <bicho44@gmail.com>
2019-10-15 09:30:17 -0300

    precio y price tag

--- template-parts/content-archive-desta-news.php
+++ template-parts/content-archive-desta-news.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Template part for displaying the destacados posts.
  *
@@ -10,17 +11,18 @@
 
 <?php
 // Must be inside a loop.
-if (has_post_thumbnail()) {?>
-<!-- <a href="<?php //the_permalink();?>"> -->
-<?php  /* @todo Cambiar para que la imagen sea responsive */
+if (has_post_thumbnail()) { ?>
+    <!-- <a href="<?php //the_permalink();
+                        ?>"> -->
+    <?php  /* @todo Cambiar para que la imagen sea responsive */
         the_post_thumbnail('news-archive');
         ?>
-<!-- </a> -->
+    <!-- </a> -->
 <?php } ?>
 
 <h3>
     <a href="<?php the_permalink(); ?>" rel="bookmark">
-        <?php the_title();?>
+        <?php the_title(); ?>
     </a>
 </h3>
 
@@ -28,36 +30,38 @@ if (has_post_thumbnail()) {?>
     <?php shortentext(get_the_content(), 100);  ?>
 </div>
 <?php $link_compra = get_post_meta(get_the_ID(), 'imgd_link_compra_field', true); ?>
-<!-- <pre><?php //var_dump($link_compra);?>
+<!-- <pre><?php //var_dump($link_compra);
+            ?>
 </pre> -->
-
+<?php
+if ($price = imgd_get_the_price_by_season(get_the_ID())) {
+    echo "<span class='pricetag2'>$ " . $price . "</span>";
+}
+?>
 
 <div class="botones">
-    <a href="#consulta-<?php the_ID(); ?>"
-        class="btn btn--consulta"><?php _e('Consulta por esta actividad', 'imgd'); ?></a>
-    <?php if ($link_compra!="") { ?>
-    <a href="<?php echo $link_compra; ?>"
-        class="btn btn--vermas"><?php echo get_post_meta(get_the_ID(), 'imgd_texto_link_compra_field', true);?></a>
+    <a href="#consulta-<?php the_ID(); ?>" class="btn btn--consulta"><?php _e('Consulta por esta actividad', 'imgd'); ?></a>
+    <?php if ($link_compra != "") { ?>
+        <a href="<?php echo $link_compra; ?>" class="btn btn--vermas"><?php echo get_post_meta(get_the_ID(), 'imgd_texto_link_compra_field', true); ?></a>
     <?php }; ?>
 
-    <!-- Acá va el Modal de la consulta por <?php the_title();?> -->
-    <section class="modal--show" id="consulta-<?php the_ID(); ?>"
-        tabindex="-1" role="dialog" aria-labelledby="modal-label" aria-hidden="true">
+    <!-- Acá va el Modal de la consulta por <?php the_title(); ?> -->
+    <section class="modal--show" id="consulta-<?php the_ID(); ?>" tabindex="-1" role="dialog" aria-labelledby="modal-label" aria-hidden="true">
 
         <div class="modal-inner">
             <header>
-                <h3 id="label-fade">Consulta por <?php the_title();?>
+                <h3 id="label-fade">Consulta por <?php the_title(); ?>
                 </h3>
             </header>
             <div class="modal-content">
                 <?php
-            /**
-             * @todo: hacer un setting para el shortcode
-             * @todo: buscar una solución más elegante a tener que madar un do_shortcode
-             * @todo: Buscar como mandar un mail mas personalizado de consulta por ejemplo el "subject" del mail y
-             *        dentro del cuerpo del mismo algo más adaptado a lo que necesita una consulta
-            */
-            echo do_shortcode('[contact-form-7 id="100" title="consulta"]') ?>
+                /**
+                 * @todo: hacer un setting para el shortcode
+                 * @todo: buscar una solución más elegante a tener que madar un do_shortcode
+                 * @todo: Buscar como mandar un mail mas personalizado de consulta por ejemplo el "subject" del mail y
+                 *        dentro del cuerpo del mismo algo más adaptado a lo que necesita una consulta
+                 */
+                echo do_shortcode('[contact-form-7 id="100" title="consulta"]') ?>
             </div>
         </div>
 
--- template-parts/content-front/destacados-actividades.php
+++ template-parts/content-front/destacados-actividades.php
@@ -4,48 +4,47 @@
 
 
 // Acá seleciono las Páginas que voy a mostrar como destacados en la Home
-$args = array('post_type' => array('imgd_actividad'),
-'post_status' => 'publish',
-'posts_per_page' => $actividad_cant,
-'ignore_sticky_posts'=>true
+$args = array(
+  'post_type' => array('imgd_actividad'),
+  'post_status' => 'publish',
+  'posts_per_page' => $actividad_cant,
+  'ignore_sticky_posts' => true
 );
 
 $loop = new WP_Query($args);
 
 
 
-if ($loop->have_posts()) {?>
-<section class="section-actividades">
-  <h3><?php
-      _e('Nuestras Actividades', 'imgd');
-        //echo the_archive_title( "<h1>", "</h1>");
-        //echo the_archive_description( '<div class="archive-description">', '</div>' );
-            ?>
-  </h3>
-  <?php
+if ($loop->have_posts()) { ?>
+  <section class="section-actividades">
+    <h3><?php
+          _e('Nuestras Actividades', 'imgd');
+          //echo the_archive_title( "<h1>", "</h1>");
+          //echo the_archive_description( '<div class="archive-description">', '</div>' );
+          ?>
+    </h3>
+    <?php
       $x = 0;
       $destacadosnewsID = array();
-?>
+      ?>
 
 
-  <div class="actividades">
-    <?php
-    while ($loop->have_posts()) : $loop->the_post();
-
-      $destacadosnewsID[] = get_the_ID();
-?>
-    <article id="post-<?php the_ID(); ?>" class="actividad">
+    <div class="actividades">
       <?php
-          get_template_part('template-parts/content-archive', 'desta-news'); ?>
+        while ($loop->have_posts()) : $loop->the_post();
 
-    </article> <!-- End Actividad -->
+          $destacadosnewsID[] = get_the_ID();
+          ?>
+        <article id="post-<?php the_ID(); ?>" class="actividad">
+          <?php
+              get_template_part('template-parts/content-archive', 'desta-news'); ?>
+        </article> <!-- End Actividad -->
 
+        <?php $x++; ?>
+      <?php endwhile; ?>
 
-    <?php $x++; ?>
-    <?php endwhile; ?>
-
-  </div>
-</section>
+    </div>
+  </section>
 <?php } ?>
 
 <?php wp_reset_query();
